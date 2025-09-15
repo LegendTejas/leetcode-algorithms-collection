08a6ddf17d553c0d2c34c7c0e21ee5a4a4ba219f
Tejas Tp <tejastp193@gmail.com>
2025-09-15 07:36:27 +0530

    Add insert function for merging intervals

---
+++ Intervals & Sweep Line/57_Insert_Interval.py
@@ -0,0 +1,25 @@
+"""
+LeetCode 57. Insert Interval
+----------------------------
+Insert a new interval into a list of non-overlapping sorted intervals 
+and merge if necessary.
+
+Approach:
+- Add intervals before/after newInterval directly
+- Merge overlaps into one interval
+"""
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort()
+
+        res = [intervals[0]]
+
+        for i in range(1, len(intervals)):
+            if res[-1][1] >= intervals[i][0]:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
+            else:
+                res.append(intervals[i])
+
+        return res
